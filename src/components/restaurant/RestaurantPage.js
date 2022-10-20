493a8f7f013fdf9e3d66eb14a7fdeca32857caab
HARSH0005 <harshdeep4567@gmail.com>
2022-10-20 18:51:10 +0530

    update api url

--- src/components/restaurant/RestaurantPage.js
+++ src/components/restaurant/RestaurantPage.js
@@ -4,6 +4,8 @@ import Header from "../Header";
 import { useParams } from "react-router-dom";
 import jwt_decode from 'jwt-decode';
 import Swal from 'sweetalert2'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 
 
@@ -171,6 +173,32 @@ razorpayObject.open();
   }, []);
   return (
     <>
+
+<div
+        className="modal fade"
+        id="slideShow"
+        tabIndex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg " style={{ height: "75vh" }}>
+          <div className="modal-content">
+            <div className="modal-body h-75">
+              <Carousel showThumbs={false} infiniteLoop={true}>
+                {restaurant.thumb.map((value, index) => {
+                  return (
+                    <div key={index} className="w-100">
+                      <img src={"/images/" + value} />
+                    </div>
+                  );
+                })}
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
       <div
         className="modal fade"
         id="exampleModalToggle"
@@ -332,7 +360,8 @@ razorpayObject.open();
       <section className="row justify-content-center">
         <section className="col-11 mt-2 restaurant-main-image position-relative">
           <img src={"/images/" + restaurant.image} alt="" />
-          <button className="btn-gallery position-absolute btn">
+          <button className="btn-gallery position-absolute btn" data-bs-toggle="modal"
+            data-bs-target="#slideShow">
             Click to see Image Gallery
           </button>
         </section>
